5334b0d0df02d18214c13528d130d629e200bd7e
Jonathan N Alencar <56272660+jonathannalencar@users.noreply.github.com>
2023-09-29 10:28:53 -0300

    Create principal.java

---
+++ principal.java
@@ -0,0 +1,19 @@
+package aula2909;
+
+import java.util.Scanner;
+
+public class principal {
+	public static void main(String[] args) {
+		Scanner rc = new Scanner(System.in);
+		System.out.println("Insira a quantidade de alunos na turma: ");
+		int quant = rc.nextInt();
+		float notas[] = new float[quant];
+		float total = 0;
+		for(int i = 0; i<quant; i++) {
+			System.out.print("Insira a nota do aluno "+(i+1)+": ");
+			notas[i]= rc.nextFloat();
+			total+=notas[i];
+		}
+		System.out.println("A media da turma é de: "+total/quant);
+	}
+}
