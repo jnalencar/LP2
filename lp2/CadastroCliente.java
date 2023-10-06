a5deddad01a2f5ba1b8f85b875f4d96813938f4e
Jonathan N Alencar <56272660+jonathannalencar@users.noreply.github.com>
2023-10-06 11:01:55 -0300

    Update CadastroCliente.java

--- lp2/CadastroCliente.java
+++ lp2/CadastroCliente.java
@@ -3,33 +3,79 @@ package lp2;
 import java.util.Scanner;
 
 public class CadastroCliente {
-    public static void main(String[] args) {
-        System.out.print("Informe a quantidade de clientes:");
-        Scanner rc = new Scanner(System.in);
-        int quant = rc.nextInt();
-        while(quant>50){
-            System.out.println("Quantidade de clientes não pode ser maior que 50");
-            System.out.print("Informe a quantidade de clientes:");
-            quant = rc.nextInt();
-        }
-        Cliente[] listaCliente = new Cliente[quant];
-        for(int i = 0; i<listaCliente.length; i++){
-            listaCliente[i] = new Cliente();
-            System.out.print("Informe código do cliente: ");
-            listaCliente[i].setCodigo(rc.nextInt());
-            System.out.print("Informe nome do cliente: ");
-            listaCliente[i].setNome(rc.next());
-            System.out.print("Informe CPF do cliente: ");
-            listaCliente[i].setCpf(rc.nextInt());
-            System.out.print("Informe telefone do cliente: ");
-            listaCliente[i].setTelefone(rc.next());
-            System.out.println("");
-        }
-        
+	public static Cliente cadastraClientes() {
+		Scanner rc = new Scanner(System.in);
+        Cliente novoCliente = new Cliente();
+        System.out.print("Informe código do cliente: ");
+        novoCliente.setCodigo(rc.nextInt());
+        System.out.print("Informe nome do cliente: ");
+        novoCliente.setNome(rc.next());
+        System.out.print("Informe CPF do cliente: ");
+        novoCliente.setCpf(rc.nextInt());
+        System.out.print("Informe telefone do cliente: ");
+        novoCliente.setTelefone(rc.next());
+        System.out.println("");
         rc.close();
-        System.out.println("\nForam cadastrados "+quant+" clientes");
-        for(int i = 0; i<listaCliente.length; i++){
-            System.out.println(listaCliente[i]);
+        return novoCliente;
+	}
+	
+	public static boolean buscaCliente(String nome, Cliente[] listaClientes) {
+		boolean j = false;
+		for(int i = 0; i < listaClientes.length; i++) {
+			if(listaClientes[i].getNome().equals(nome)) { //CRIAR O EQUALS
+				System.out.println(listaClientes[i]);
+				j = true;
+				
+			}
+		}
+		return j;
+	}
+	
+	public static void exibirDadosClientes(Cliente[] listaClientes, int qtdClientes) {
+		for(int i = 0; i<qtdClientes; i++) {
+			System.out.println(listaClientes[i]);
+			System.out.println("");
+		}
+	}
+	
+    public static void main(String[] args) {
+        System.out.println("Menu:");
+        System.out.println("1 - Cadastrar clientes\n2 - Buscar cliente pelo nome\n3 - Exibir dados dos clientes\n4 - Sair");
+        System.out.println("Escolha uma opção: ");
+        Scanner rc = new Scanner(System.in);
+        int opcao = rc.nextInt();
+        Cliente[] listaClientes = new Cliente[50];
+        for(int k = 0; k<50; k++) {
+        	listaClientes[k] = new Cliente();
         }
+        int qtdClientes = 0;
+        while(opcao != 4) {
+        	if(opcao == 1) {
+        		Cliente novoCliente = cadastraClientes();
+        		listaClientes[qtdClientes] = novoCliente;
+        		qtdClientes = qtdClientes + 1;
+        		//Cliente[] listaNovosClientes = cadastraClientes();
+        		//listaClientes = listaClientes + listaNovosClientes
+        	}
+        	if(opcao == 2) {
+        		System.out.print("Digite o nome do cliente que deseja buscar: ");
+        		String nomeCliente = rc.next();
+        		if(!buscaCliente(nomeCliente, listaClientes)) {
+        			System.out.println("Nenhum cliente com o nome "+nomeCliente+" foi encontrado.");
+        		}
+        	}
+        	if(opcao == 3) {
+        		//if(qtdClientes > 0) {
+        			exibirDadosClientes(listaClientes, qtdClientes);
+        		//}
+        		//else {
+        			//System.out.println("Nenhum cliente cadastrado");
+        		//}
+        	}
+        	System.out.println("Escolha uma opção: ");
+        	opcao = rc.nextInt();
+        	
+        }
+        rc.close();
     }
 }
